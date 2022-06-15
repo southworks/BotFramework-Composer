43d96cf44391076fa275a2c950b95e3ba2ed238a
sw-joelmut <joel.mut@southworks.com>
2022-06-15 14:47:31 -0300

    test

--- Composer/packages/integration-tests/cypress/integration/DesignPage.spec.ts
+++ Composer/packages/integration-tests/cypress/integration/DesignPage.spec.ts
@@ -3,6 +3,20 @@
 
 // Tests for dialog & trigger CRUD
 
+const COMMAND_DELAY = 500;
+
+for (const command of ['visit', 'click', 'trigger', 'type', 'clear', 'reload', 'contains']) {
+  Cypress.Commands.overwrite(command, (originalFn, ...args) => {
+    const origVal = originalFn(...args);
+
+    return new Promise((resolve) => {
+      setTimeout(() => {
+        resolve(origVal);
+      }, COMMAND_DELAY);
+    });
+  });
+}
+
 context('breadcrumb', () => {
   beforeEach(() => {
     cy.createTestBot('TestSample', ({ id }) => {
