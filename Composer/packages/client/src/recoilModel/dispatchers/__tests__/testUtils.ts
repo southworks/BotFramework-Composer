0f0e75e43e4ce2e7dc449b714158ede916f32a9c
CeciliaAvila <cecilia.avila@southworks.com>
2023-01-19 10:09:10 -0300

    Update recoil package

--- Composer/packages/client/src/recoilModel/dispatchers/__tests__/testUtils.ts
+++ Composer/packages/client/src/recoilModel/dispatchers/__tests__/testUtils.ts
@@ -9,12 +9,21 @@ export const mockCallback: CallbackInterface = {
   set: mockSet,
   reset: jest.fn(),
   gotoSnapshot: jest.fn(),
+  refresh: jest.fn(),
+  // eslint-disable-next-line @typescript-eslint/camelcase
+  transact_UNSTABLE: jest.fn(),
   snapshot: {
     getID: jest.fn(),
     getLoadable: jest.fn(),
     getPromise: jest.fn(),
     map: jest.fn(),
     asyncMap: jest.fn(),
+    // eslint-disable-next-line @typescript-eslint/camelcase
+    getNodes_UNSTABLE: jest.fn(),
+    // eslint-disable-next-line @typescript-eslint/camelcase
+    getInfo_UNSTABLE: jest.fn(),
+    retain: jest.fn(),
+    isRetained: jest.fn(),
   },
 };
 
